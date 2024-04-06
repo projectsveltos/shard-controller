b6b037f0936e9a36f92f57b663c92a1eb691b834
mgianluc <mgianluca@cisco.com>
2024-04-06 10:22:45 +0200

    Fix manifest
    Event-manager and Healthcheck-manager main branches where not
    updated yet after v0.27.0 release.
    
    This PR fixes shard-controller manifest by rebuilding it

--- pkg/sharding/event-manager.go
+++ pkg/sharding/event-manager.go
@@ -42,7 +42,7 @@ spec:
         - --v=5
         command:
         - /manager
-        image: projectsveltos/event-manager-amd64:main
+        image: projectsveltos/event-manager:main
         livenessProbe:
           failureThreshold: 3
           httpGet:
