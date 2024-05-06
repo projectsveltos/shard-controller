aface3b45d27b12b73f5167903dc9a13f24a6113
mgianluc <mgianluca@cisco.com>
2024-05-06 19:23:55 +0200

    Prepare dev to main merge
    Post release v0.29.0

--- pkg/sharding/event-manager.go
+++ pkg/sharding/event-manager.go
@@ -42,7 +42,7 @@ spec:
         - --v=5
         command:
         - /manager
-        image: projectsveltos/event-manager:v0.29.0
+        image: projectsveltos/event-manager:main
         livenessProbe:
           failureThreshold: 3
           httpGet:
