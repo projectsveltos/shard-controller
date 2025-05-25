1a348954f84d9f03b7a245db4d6c51c709e55e4c
Gianluca Mardente <gianluca.mardente@gmail.com>
2025-05-25 18:40:00 +0200

    (chore) set tag to v0.55.0

--- pkg/sharding/healthcheck-manager.go
+++ pkg/sharding/healthcheck-manager.go
@@ -41,11 +41,11 @@ spec:
         - --shard-key={{.SHARD}}
         - --capi-onboard-annotation=
         - --v=5
-        - --version=main
+        - --version=v0.55.0
         - --agent-in-mgmt-cluster=false
         command:
         - /manager
-        image: docker.io/projectsveltos/healthcheck-manager@sha256:9f8f60a5f66d15b998c730706c031d993bda55277e4942a008d4000601c2eb8a
+        image: docker.io/projectsveltos/healthcheck-manager@sha256:3a91761cc3981b8caf10ace7a4d02a90174979107767fe9098338a2507e3d2b6
         livenessProbe:
           failureThreshold: 3
           httpGet:
